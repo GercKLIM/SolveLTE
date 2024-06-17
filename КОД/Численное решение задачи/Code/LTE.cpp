226c78ae49c3283136137f0c78a3c024e682c2d1
GercKLIM <gerceg0808@yandex.ru>
2024-06-17 12:31:50 +0300

    V.2.1

--- КОД/Численное решение задачи/Code/LTE.cpp
+++ КОД/Численное решение задачи/Code/LTE.cpp
@@ -71,211 +71,85 @@ bool LTE::SolveLD2e(std::string filename){
     }
 }
 
-//// Функция для решения трехдиагональной системы уравнений
-//std::vector<double> LTE::SolveTriDiagonal(const std::vector<std::vector<double>>& A, const std::vector<double>& B) {
-//    int n = B.size();
-//    std::vector<double> x(n);
-//    std::vector<double> alpha(n), beta(n);
-//
-//    alpha[0] = A[0][1] / A[0][0];
-//    beta[0] = B[0] / A[0][0];
-//
-//    for (int i = 1; i < n - 1; ++i) {
-//        double m = 1.0 / (A[i][i] - A[i][i - 1] * alpha[i - 1]);
-//        alpha[i] = A[i][i + 1] * m;
-//        beta[i] = (B[i] - A[i][i - 1] * beta[i - 1]) * m;
-//    }
-//
-//    x[n - 1] = (B[n - 1] - A[n - 1][n - 2] * beta[n - 2]) / (A[n - 1][n - 1] - A[n - 1][n - 2] * alpha[n - 2]);
-//    for (int i = n - 2; i >= 0; --i) {
-//        x[i] = alpha[i] * x[i + 1] + beta[i];
-//    }
-//
-//    return x;
-//}
-//
-//// Неявная схема с левой разностью 1-го пор.
-//bool LTE::SolveLD2i(std::string filename) {
-//    // Открытие файла
-//    std::ofstream file(filename);
-//    if (!file.is_open()) {
-//        std::cout << "LOG: Couldn't open file: " << filename << std::endl;
-//        return false;
-//    }
-//
-//    // Создаем сетки
-//    int Nx = static_cast<int>(abs(l2 - l1) / h);
-//    int Nt = static_cast<int>(abs(T - t0) / tau);
-//    std::vector<double> web_h(Nx + 1);
-//    std::vector<double> web_tau(Nt + 1);
-//
-//    for (int x = 0; x <= Nx; x++) {
-//        web_h[x] = l1 + x * h;
-//    }
-//
-//    for (int time = 0; time <= Nt; time++) {
-//        web_tau[time] = t0 + time * tau;
-//    }
-//
-//    // Запись в файл первой строчки с шагом по пространству
-//    file << "0 ";
-//    for (int x = 0; x <= Nx; x++) {
-//        file << web_h[x] << " ";
-//    }
-//    file << std::endl;
-//
-//    std::vector<double> U_prev(Nx + 1);
-//    std::vector<double> U_now(Nx + 1);
-//
-//    // Аппроксимация начального условия
-//    for (int x = 0; x <= Nx; x++) {
-//        U_prev[x] = U0(web_h[x]);
-//    }
-//    U_now = U_prev;
-//
-//    // Запись первой итерации <-> начального условия
-//    file << t0 << " ";
-//    for (int x = 0; x <= Nx; x++) {
-//        file << U_prev[x] << " ";
-//    }
-//    file << std::endl;
-//
-//    // Итерационный процесс
-//    for (int time = 1; time <= Nt; time++) {
-//        // Формирование матрицы и вектора для неявной схемы
-//        std::vector<std::vector<double>> A(Nx + 1, std::vector<double>(Nx + 1, 0.0));
-//        std::vector<double> B(Nx + 1);
-//
-//        double c = -gamma / h;
-//        double b = 1.0 + gamma / h;
-//
-//        for (int x = 0; x <= Nx; x++) {
-//            if (x == 0) {
-//                A[x][x] = b;
-//                A[x][x + 1] = c;
-//                B[x] = U_prev[x];
-//            } else if (x == Nx) {
-//                A[x][x - 1] = c;
-//                A[x][x] = b;
-//                B[x] = U_prev[x];
-//            } else {
-//                A[x][x - 1] = c;
-//                A[x][x] = b;
-//                A[x][x + 1] = c;
-//                B[x] = U_prev[x];
-//            }
-//        }
-//
-//        // Решение системы уравнений
-//        std::vector<double> U_next = SolveTriDiagonal(A, B);
-//
-//        // Обновление значений для следующего временного шага
-//        U_prev = U_next;
-//
-//        // Запись в файл
-//        file << web_tau[time] << " ";
-//        for (int x = 0; x <= Nx; x++) {
-//            file << U_next[x] << " ";
-//        }
-//        file << std::endl;
-//    }
-//
-//    file.close();
-//    std::cout << "LOG: Implicit method complete! Gamma = " << gamma << " Result saved on: " << filename << std::endl;
-//    return true;
-//}
 
-// Неявная схема с левой разностью 1-го порядка
+
 bool LTE::SolveLD2i(std::string filename) {
-    // Открытие файла
+
     std::ofstream file(filename);
-    if (!file.is_open()) {
+    if (file.is_open()) {
+
+        int Nx = static_cast<int>(abs(l2 - l1) / h);
+        int Nt = static_cast<int>(abs(T - t0) / tau);
+        std::vector<double> web_h(Nx + 1);
+        std::vector<double> web_tau(Nt + 1);
+
+        for (int x = 0; x <= Nx; x++) {
+            web_h[x] = l1 + x * h;
+        }
+
+        for (int time = 0; time <= Nt; time++) {
+            web_tau[time] = t0 + time * tau;
+        }
+
+        file << "0 ";
+        for (int x = 0; x <= Nx; x++) {
+            file << web_h[x] << " ";
+        }
+        file << std::endl;
+
+        std::vector<double> U_prev(Nx + 1);
+        std::vector<double> U_now(Nx + 1);
+
+        for (int x = 0; x <= Nx; x++) {
+            U_prev[x] = U0(web_h[x]);
+        }
+        U_now = U_prev;
+
+        file << t0 << " ";
+        for (int x = 0; x <= Nx; x++) {
+            file << U_prev[x] << " ";
+        }
+        file << std::endl;
+
+        for (int time = 1; time <= Nt; time++) {
+            std::vector<std::vector<double>> A(Nx + 1, std::vector<double>(Nx + 1, 0.0));
+            std::vector<double> B(Nx + 1);
+
+            for (int x = 1; x < Nx; x++) {
+                A[x][x - 1] = -gamma;
+                A[x][x] = 1 + gamma;
+                B[x] = U_prev[x];
+            }
+
+            // Граничное условие слева, задаваемое функцией U_left
+            A[0][0] = 1.0;
+            B[0] = GU_left(web_tau[time]);
+
+            // Правое граничное условие периодическое
+            A[Nx][Nx] = 1.0;
+            B[Nx] = U_prev[Nx];
+
+            std::vector<double> U_next = SolveTriDiagonal(A, B);
+
+            U_prev = U_next;
+
+            file << web_tau[time] << " ";
+            for (int x = 0; x <= Nx; x++) {
+                file << U_next[x] << " ";
+            }
+            file << std::endl;
+        }
+
+        file.close();
+        std::cout << "LOG: Implicit method complete! Gamma = " << gamma << " Result saved on: " << filename
+                  << std::endl;
+        return true;
+    } else {
         std::cout << "LOG: Couldn't open file: " << filename << std::endl;
         return false;
     }
-
-    // Создаем сетки
-    int Nx = static_cast<int>(abs(l2 - l1) / h);
-    int Nt = static_cast<int>(abs(T - t0) / tau);
-    std::vector<double> web_h(Nx + 1);
-    std::vector<double> web_tau(Nt + 1);
-
-    for (int x = 0; x <= Nx; x++) {
-        web_h[x] = l1 + x * h;
-    }
-
-    for (int time = 0; time <= Nt; time++) {
-        web_tau[time] = t0 + time * tau;
-    }
-
-    // Запись в файл первой строчки с шагом по пространству
-    file << "0 ";
-    for (int x = 0; x <= Nx; x++) {
-        file << web_h[x] << " ";
-    }
-    file << std::endl;
-
-    std::vector<double> U_prev(Nx + 1);
-    std::vector<double> U_now(Nx + 1);
-
-    // Аппроксимация начального условия
-    for (int x = 0; x <= Nx; x++) {
-        U_prev[x] = U0(web_h[x]);
-    }
-    U_now = U_prev;
-
-    // Запись первой итерации <-> начального условия
-    file << t0 << " ";
-    for (int x = 0; x <= Nx; x++) {
-        file << U_prev[x] << " ";
-    }
-    file << std::endl;
-
-    // Итерационный процесс
-    for (int time = 1; time <= Nt; time++) {
-        // Формирование матрицы и вектора для неявной схемы
-        std::vector<std::vector<double>> A(Nx + 1, std::vector<double>(Nx + 1, 0.0));
-        std::vector<double> B(Nx + 1);
-
-        double aa = -gamma / h;
-        double b = 1.0 + gamma / h;
-
-        for (int x = 0; x <= Nx; x++) {
-            if (x == 0) {
-                A[x][x] = 1.0; // Граничное условие
-                B[x] = U_prev[x]; // Задаем граничное условие
-            } else if (x == Nx) {
-                A[x][x] = 1.0; // Граничное условие
-                B[x] = U_prev[x]; // Задаем граничное условие
-            } else {
-                A[x][x - 1] = aa;
-                A[x][x] = b;
-                A[x][x + 1] = 0.0; // Не учитываем правую часть
-                B[x] = U_prev[x];
-            }
-        }
-
-        // Решение системы уравнений
-        std::vector<double> U_next = SolveTriDiagonal(A, B);
-
-        // Обновление значений для следующего временного шага
-        U_prev = U_next;
-
-        // Запись в файл
-        file << web_tau[time] << " ";
-        for (int x = 0; x <= Nx; x++) {
-            file << U_next[x] << " ";
-        }
-        file << std::endl;
-    }
-
-    file.close();
-    std::cout << "LOG: Implicit method complete! Gamma = " << gamma << " Result saved on: " << filename << std::endl;
-    return true;
 }
 
-
-// Функция для решения трехдиагональной системы уравнений
 std::vector<double> LTE::SolveTriDiagonal(const std::vector<std::vector<double>>& A, const std::vector<double>& B) {
     int n = B.size();
     std::vector<double> x(n);
@@ -300,6 +174,8 @@ std::vector<double> LTE::SolveTriDiagonal(const std::vector<std::vector<double>>
 
     return x;
 }
+
+
 
 // Явная схема с левой разностью на 3 точках
 bool LTE::SolveLD3e(std::string filename){
